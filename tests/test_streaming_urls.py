a408d2aa68a1d2e5a4fc5e58affed507ce9ad228
Brian Hannafious <bhannafi@ucsc.edu>
2021-04-29 09:59:09 -0700

    Remove test duration tests, fix coverage

--- tests/test_streaming_urls.py
+++ tests/test_streaming_urls.py
@@ -3,7 +3,6 @@ import io
 import os
 import sys
 import time
-import requests
 import unittest
 import contextlib
 from math import ceil
@@ -14,7 +13,7 @@ from random import randint
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
-import streaming_urls as su
+from streaming_urls import Reader, for_each_part, for_each_part_async
 
 from tests.infra import GS, S3, suppress_warnings
 
@@ -40,23 +39,13 @@ class TestStreamingURLsReader(unittest.TestCase):
         GS.bucket.blob(cls.key).upload_from_file(io.BytesIO(cls.expected_data))
         cls.gs_url = GS.generate_presigned_GET_url(cls.key)
 
-    def duration_subtests(self, test_concurrency=[None, 4]):
-        print()
-        for concurrency in test_concurrency:
-            start_time = time.time()
-            try:
-                yield concurrency
-            except GeneratorExit:
-                return
-            print(self.id(), "duration", f"concurrency={concurrency}", time.time() - start_time)
-
     def test_reader_interface(self):
         with contextlib.ExitStack() as stack:
             stack.enter_context(mock.patch("streaming_urls.http.Session.size"))
             stack.enter_context(mock.patch("streaming_urls.reader.SharedCircularBuffer"))
             stack.enter_context(mock.patch("streaming_urls.reader.ProcessPoolExecutor"))
             stack.enter_context(mock.patch("streaming_urls.reader.ConcurrentQueue"))
-            reader = su.Reader("some_url")
+            reader = Reader("some_url")
             with self.assertRaises(OSError):
                 reader.fileno()
             with self.assertRaises(OSError):
@@ -79,13 +68,13 @@ class TestStreamingURLsReader(unittest.TestCase):
 
     def test_read(self):
         for platform, url in [("aws", self.s3_url), ("gcp", self.gs_url)]:
-            for concurrency in self.duration_subtests([None, 3]):
+            for concurrency in (None, 3):
                 with self.subTest(platform=platform, concurrency=concurrency):
                     self._test_read(url, concurrency)
 
     def _test_read(self, url: str, concurrency: int, chunk_size=None):
         chunk_size = chunk_size or len(self.expected_data) // 5
-        with su.Reader(url, chunk_size, concurrency=concurrency) as reader:
+        with Reader(url, chunk_size, concurrency=concurrency) as reader:
             data = bytearray()
             while True:
                 d = reader.read(randint(chunk_size // 3, chunk_size))
@@ -100,7 +89,7 @@ class TestStreamingURLsReader(unittest.TestCase):
         Ensure it is not possible to overlap the circular buffer.
         """
         chunk_size = len(self.expected_data) // 5
-        with su.Reader(self.gs_url, chunk_size, concurrency=2) as reader:
+        with Reader(self.gs_url, chunk_size, concurrency=2) as reader:
             view = reader.read(1)
             expected_first_byte = bytes(view)
             try:
@@ -120,21 +109,21 @@ class TestStreamingURLsReader(unittest.TestCase):
     def test_readinto(self):
         chunk_size = len(self.expected_data) // 3
         for platform, url in [("aws", self.s3_url), ("gcp", self.gs_url)]:
-            for concurrency in self.duration_subtests([None, 3, 5]):
+            for concurrency in (None, 3, 5):
                 with self.subTest(platform=platform, concurrency=concurrency):
                     self._test_readinto(url, concurrency, chunk_size)
 
     def _test_readinto(self, url: str, concurrency: int, chunk_size: int):
         buff = bytearray(2 * len(self.expected_data))
-        with su.Reader(self.s3_url, chunk_size=chunk_size, concurrency=concurrency) as fh:
+        with Reader(self.s3_url, chunk_size=chunk_size, concurrency=concurrency) as fh:
             bytes_read = fh.readinto(buff)
             self.assertEqual(self.expected_data[:bytes_read], buff[:bytes_read])
 
     def test_for_each_part(self):
         chunk_size = len(self.expected_data) // 10
-        for concurrency in self.duration_subtests([None, 2]):
+        for concurrency in (None, 2):
             data = bytearray()
-            for chunk in su.for_each_part(self.gs_url, chunk_size=chunk_size, concurrency=concurrency):
+            for chunk in for_each_part(self.gs_url, chunk_size=chunk_size, concurrency=concurrency):
                 data += chunk
                 chunk.release()
             self.assertEqual(data, self.expected_data)
@@ -143,9 +132,9 @@ class TestStreamingURLsReader(unittest.TestCase):
         chunk_size = len(self.expected_data) // 50
         number_of_chunks = ceil(len(self.expected_data) / chunk_size)
 
-        for concurrency in self.duration_subtests(test_concurrency=[8, 10]):
+        for concurrency in (8, 10):
             data = bytearray(len(self.expected_data))
-            for chunk_id, chunk in su.for_each_part_async(self.gs_url,
+            for chunk_id, chunk in for_each_part_async(self.gs_url,
                                                           chunk_size=chunk_size,
                                                           concurrency=concurrency):
                 data[chunk_id * chunk_size: chunk_id * chunk_size + len(chunk)] = chunk
