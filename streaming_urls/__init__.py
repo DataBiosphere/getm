6f999f45e26184d5523ebd9d82b815638725a7fd
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2021-05-01 14:03:49 -0600

    Use simpler interface for url content iterators (#13)

--- streaming_urls/__init__.py
+++ streaming_urls/__init__.py
@@ -1,8 +1,7 @@
 from typing import Generator, Optional, Tuple
 
 from streaming_urls import config
-from streaming_urls.reader import (BaseURLReader, URLRawReader, URLReader, for_each_part, for_each_part_raw,
-                                   for_each_part_unordered)
+from streaming_urls.reader import BaseURLReader, URLRawReader, URLReader, iter_content_unordered
 
 
 def urlopen(url: str,
@@ -17,11 +16,6 @@ def iter_content(url: str,
                  chunk_size: int=config.default_chunk_size,
                  concurrency: Optional[int]=config.default_concurrency) -> Generator[memoryview, None, None]:
     if concurrency is None:
-        return for_each_part_raw(url, chunk_size)
+        return URLRawReader.iter_content(url, chunk_size)
     else:
-        return for_each_part(url, chunk_size, concurrency)
-
-def iter_content_unordered(url: str,
-                           chunk_size: int=config.default_chunk_size,
-                           concurrency: int=config.default_concurrency) -> Generator[Tuple[int, memoryview], None, None]:  # noqa
-    return for_each_part_unordered(url, chunk_size, concurrency)
+        return URLReader.iter_content(url, chunk_size, concurrency)
