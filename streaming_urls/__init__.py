4e6b4da17441e25c6a8573326725142b7cd55f33
Brian Hannafious <bhannafi@ucsc.edu>
2021-05-01 10:22:27 -0700

    Improve interface for part iterators

--- streaming_urls/__init__.py
+++ streaming_urls/__init__.py
@@ -1,6 +1,8 @@
+from typing import Generator, Optional, Tuple
+
 from streaming_urls import config
-from streaming_urls.reader import URLRawReader, URLReader, iter_content, iter_content_unordered
-from typing import Optional
+from streaming_urls.reader import URLRawReader, URLReader, for_each_part, for_each_part_raw, for_each_part_unordered
+
 
 def urlopen(url: str,
             chunk_size: int=config.default_chunk_size,
@@ -9,3 +11,16 @@ def urlopen(url: str,
         return URLRawReader(url)
     else:
         return URLReader(url, chunk_size, concurrency)
+
+def iter_content(url: str,
+                 chunk_size: int=config.default_chunk_size,
+                 concurrency: Optional[int]=config.default_concurrency) -> Generator[memoryview, None, None]:
+    if concurrency is None:
+        return for_each_part_raw(url, chunk_size)
+    else:
+        return for_each_part(url, chunk_size, concurrency)
+
+def iter_content_unordered(url: str,
+                           chunk_size: int=config.default_chunk_size,
+                           concurrency: int=config.default_concurrency) -> Generator[Tuple[int, memoryview], None, None]:  # noqa
+    return for_each_part_unordered(url, chunk_size, concurrency)
