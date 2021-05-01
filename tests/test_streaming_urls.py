9320a372d73ffd6d39ee1d2e991ca400578fa942
Brian Hannafious <bhannafi@ucsc.edu>
2021-05-01 10:47:54 -0700

    Add dispatch test

--- tests/test_streaming_urls.py
+++ tests/test_streaming_urls.py
@@ -38,6 +38,29 @@ class TestStreamingURLs(unittest.TestCase):
         GS.bucket.blob(cls.key).upload_from_file(io.BytesIO(cls.expected_data))
         cls.gs_url = GS.generate_presigned_GET_url(cls.key)
 
+    def test_dispatch(self):
+        with contextlib.ExitStack() as stack:
+            stack.enter_context(mock.patch("streaming_urls.reader.SharedCircularBuffer"))
+            stack.enter_context(mock.patch("streaming_urls.reader.ProcessPoolExecutor"))
+            stack.enter_context(mock.patch("streaming_urls.reader.ConcurrentQueue"))
+            stack.enter_context(mock.patch("streaming_urls.reader.ConcurrentPool"))
+            stack.enter_context(mock.patch("streaming_urls.http.Session.size"))
+            stack.enter_context(mock.patch("streaming_urls.http.Session.raw"))
+
+            tests = [(None, streaming_urls.URLRawReader),
+                     (4, streaming_urls.URLReader)]
+            for concurrency, expected_class in tests:
+                with self.subTest(concurrency=concurrency, expected_class=expected_class):
+                    obj = streaming_urls.urlopen("http://this-is-fake-i-hope-xyz", concurrency=concurrency)
+                    self.assertIsInstance(obj, expected_class)
+
+            tests = [(None, streaming_urls.for_each_part_raw),
+                     (1, streaming_urls.for_each_part)]
+            for concurrency, expected_func in tests:
+                with self.subTest(concurrency=concurrency, expected_func=expected_func):
+                    obj = streaming_urls.iter_content("http://this-is-fake-i-hope-xyz", concurrency=concurrency)
+                    self.assertEqual(obj.__name__, expected_func.__name__)
+
     def test_reader_interface(self):
         with contextlib.ExitStack() as stack:
             stack.enter_context(mock.patch("streaming_urls.http.Session.size"))
