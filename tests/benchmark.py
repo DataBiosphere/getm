49134664cec07ee8392a2c5c53d827b8c3495e93
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2021-05-17 17:12:46 -0600

    More comprehensible benchmarking logic (#52)

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 import os
 import sys
-import time
 import base64
 import hashlib
 import unittest
-from random import randint
+from contextlib import contextmanager
 
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
-from getm import urlopen, iter_content, checksum, default_chunk_size, reader
+from getm import urlopen, iter_content, default_chunk_size, reader, checksum
 from tests.infra import GS, S3, suppress_warnings
+from tests.infra.profile import Profile
 
 
 def setUpModule():
@@ -23,75 +23,69 @@ def tearDownModule():
     GS.client._http.close()
 
 class TestBenchmark(unittest.TestCase):
-    def duration_subtests(self, tests):
-        print()
-        for t in tests:
-            start_time = time.time()
-            try:
-                with self.subTest(t[0]):
-                    yield t
-            except GeneratorExit:
-                return
-            print(self.id(), "duration", f"{t[0]}", time.time() - start_time)
-
     @classmethod
     def setUpClass(cls):
         cls.key, cls.size = GS.put_fixture()
         cls.expected_md5 = GS.bucket.get_blob(cls.key).md5_hash
+        cls.expected_crc32c = GS.bucket.get_blob(cls.key).crc32c
 
     def setUp(self):
         self.url = GS.generate_presigned_GET_url(self.key)
 
+    def tearDown(self):
+        print()  # inserting a space between profile results improves readability
+
     def test_read(self):
-        tests = [(f"concurrency={concurrency}", concurrency) for concurrency in [None, 4]]
-        for test_name, concurrency in self.duration_subtests(tests):
-            md5 = hashlib.md5()
-            with urlopen(self.url, concurrency=concurrency) as raw:
-                md5.update(raw.read())
-                while True:
-                    d = raw.read()
-                    md5.update(d)
-                    if not d:
-                        break
-            self.assertEqual(self.expected_md5, base64.b64encode(md5.digest()).decode("utf-8"))
+        for concurrency in [None, 4]:
+            with Profile(f"{self.id()} concurrency={concurrency}"):
+                cs = checksum.GSCRC32C()
+                with urlopen(self.url, concurrency=concurrency) as raw:
+                    cs.update(raw.read())
+                    while True:
+                        d = raw.read()
+                        cs.update(d)
+                        if not d:
+                            break
+                self.assertTrue(cs.matches(self.expected_crc32c))
 
     def test_read_keep_alive(self):
-        tests = [("1", 1024 * 1024 * 1)]
-        for test_name, chunk_size in self.duration_subtests(tests):
-            md5 = hashlib.md5()
+        chunk_size = 1024 * 1024 * 1
+        with Profile(f"{self.id()} chunk_size={chunk_size}"):
+            cs = checksum.GSCRC32C()
             with urlopen(self.url, concurrency=1) as raw:
                 while True:
                     d = raw.read(chunk_size)
                     if not d:
                         break
-                    md5.update(d)
-            self.assertEqual(self.expected_md5, base64.b64encode(md5.digest()).decode("utf-8"))
+                    cs.update(d)
+            self.assertTrue(cs.matches(self.expected_crc32c))
 
     def test_iter_content(self):
-        tests = [(f"concurrency={concurrency}", concurrency) for concurrency in [None, 4]]
-        for test_name, concurrency in self.duration_subtests(tests):
-            md5 = hashlib.md5()
-            for chunk in iter_content(self.url, concurrency=concurrency):
-                md5.update(chunk)
-                chunk.release()
-            self.assertEqual(self.expected_md5, base64.b64encode(md5.digest()).decode("utf-8"))
+        for concurrency in [None, 4]:
+            with Profile(f"{self.id()} concurrency={concurrency}"):
+                cs = checksum.GSCRC32C()
+                for chunk in iter_content(self.url, concurrency=concurrency):
+                    cs.update(chunk)
+                    chunk.release()
+                self.assertTrue(cs.matches(self.expected_crc32c))
 
     def test_iter_content_keep_alive(self):
-        tests = [(f"concurrency={concurrency}", concurrency) for concurrency in [1]]
-        for test_name, concurrency in self.duration_subtests(tests):
-            md5 = hashlib.md5()
+        concurrency = 1
+        with Profile(f"{self.id()} concurrency={concurrency}"):
+            cs = checksum.GSCRC32C()
             for chunk in iter_content(self.url, concurrency=concurrency):
-                md5.update(chunk)
+                cs.update(chunk)
                 chunk.release()
-            self.assertEqual(self.expected_md5, base64.b64encode(md5.digest()).decode("utf-8"))
+            self.assertTrue(cs.matches(self.expected_crc32c))
 
     def test_iter_content_unordered(self):
         tests = [(f"concurrency={concurrency}", concurrency) for concurrency in range(2,5)]
-        for test_name, concurrency in self.duration_subtests(tests):
-            md5 = hashlib.md5()
-            for i, chunk in reader.iter_content_unordered(self.url, default_chunk_size, concurrency):
-                md5.update(chunk)
-                chunk.release()
+        for concurrency in range(3, 5):
+            with Profile(f"{self.id()} concurrency={concurrency}"):
+                cs = checksum.GSCRC32C()
+                for i, chunk in reader.iter_content_unordered(self.url, default_chunk_size, concurrency):
+                    cs.update(chunk)
+                    chunk.release()
 
 if __name__ == '__main__':
     unittest.main()
