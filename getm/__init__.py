59fecd690f21bad9d023d9b7974854481e073653
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2021-05-17 13:22:25 -0600

    Simplify download logic, prepare for batch (#50)

--- getm/__init__.py
+++ getm/__init__.py
@@ -1,9 +1,6 @@
-import os
 from typing import Generator, Optional
 
 from getm import reader
-from getm.http import http
-from getm.utils import checksum_for_url
 
 
 default_chunk_size = 128 * 1024 * 1024
@@ -25,33 +22,3 @@ def iter_content(url: str, concurrency: Optional[int]=None) -> Generator[memoryv
         return reader.URLReaderKeepAlive.iter_content(url, default_chunk_size_keep_alive)
     else:
         return reader.URLReader.iter_content(url, default_chunk_size, concurrency or default_concurrency)
-
-def download_iter_parts(url: str,
-                        filepath: str,
-                        chunk_size: Optional[int]=None,
-                        concurrency: Optional[int]=1) -> Generator[int, None, None]:
-    expected_cs, cs = checksum_for_url(url)
-    assert cs is not None
-    sz = http.size(url)
-    if not chunk_size:
-        chunk_size = default_chunk_size if 1 != concurrency else default_chunk_size_keep_alive
-    try:
-        fd = os.open(filepath, os.O_WRONLY | os.O_CREAT)
-        os.pwrite(fd, b"0", sz - 1)
-        offset = 0
-        for part in iter_content(url, concurrency):
-            try:
-                cs.update(bytes(part))
-                os.pwrite(fd, part, offset)
-                part_size = len(part)
-                offset += part_size
-            finally:
-                part.release()
-            yield part_size
-    except Exception:
-        os.remove(filepath)
-        raise
-    finally:
-        os.close(fd)
-    if expected_cs is not None:
-        assert cs.matches(expected_cs), "checksum failed!"
