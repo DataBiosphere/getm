2da507d0d421c6490da9d098dfa20d32a6160762
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2021-05-25 09:54:14 -0600

    Temp location uses same disk as target (#77)
    This prevents hard link failure during download finalize.

--- getm/utils.py
+++ getm/utils.py
@@ -5,13 +5,27 @@ from typing import Optional, Tuple, Union
 from getm.http import http
 
 
+def resolve_target(url: str, filepath: Optional[str]=None) -> str:
+    """
+    Resolve the absolute target distination for 'url' given optional 'filepath. If 'filepath' is omitted use the
+    current working directory.
+    """
+    if not filepath:
+        filepath = os.path.abspath(http.name(url))
+    else:
+        filepath = os.path.abspath(os.path.expanduser(filepath))
+        if os.path.isdir(filepath):
+            filepath = os.path.join(filepath, http.name(url))
+    return filepath
+
 class indirect_open:
     """This should be used as a context manager. Provides a file object to a temporary file. Temporary file is moved to
     'filepath' if no error occurs before close. Attempt to remove temporary file in all cases.
     """
     def __init__(self, filepath: str, tmp: Optional[str]=None):
+        assert filepath == os.path.normpath(filepath)
         self.filepath = filepath
-        self.tmp = tmp or f"/tmp/getm-{uuid4()}"
+        self.tmp = tmp or f"{os.path.dirname(filepath)}/.getm-{uuid4()}"
 
     def __enter__(self):
         self.handle = open(self.tmp, "wb", buffering=0)
---
+++ tests/test_utils.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import os
+import sys
+import unittest
+from uuid import uuid4
+from unittest import mock
+
+pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # noqa
+sys.path.insert(0, pkg_root)  # noqa
+
+from getm.utils import resolve_target
+
+
+class TestUtils(unittest.TestCase):
+    def test_resolve_target(self, *args):
+        name = f"{uuid4()}"
+        tests = [
+            (os.path.join(os.path.abspath(os.path.curdir), name), None),
+            (os.path.join(os.path.abspath(os.path.curdir), name), "."),
+            (os.path.join(os.path.abspath(os.path.curdir), "foo"), "foo"),
+            ("/doom/foo", "/doom/foo"),
+            (os.path.expanduser("~/doom/foo"), "~/doom/foo"),
+            (os.path.expanduser(f"~/{name}"), "~"),
+        ]
+        with mock.patch("getm.utils.http.name", return_value=name):
+            for expected, filepath in tests:
+                with self.subTest(expected=expected, filepath=filepath):
+                    self.assertEqual(expected, resolve_target("http://foo", filepath))
+
+if __name__ == '__main__':
+    unittest.main()
